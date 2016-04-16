4fdc534c724a1cb6f2accb03442633c378260ee1
Simon Arlott <sa.me.uk>
2016-04-16 20:58:36 +0100

    don't ignore repeated invites

--- src/mod/irc.mod/chan.c
+++ src/mod/irc.mod/chan.c
@@ -1781,9 +1781,6 @@ static int gotinvite(char *from, char *msg)
             invitee, msg, nick);
     return 1;
   }
-  if (!rfc_casecmp(last_invchan, msg))
-    if (now - last_invtime < 30)
-      return 0; /* Two invites to the same channel in 30 seconds? */
   putlog(LOG_MISC, "*", "%s!%s invited me to %s", nick, from, msg);
   strlcpy(last_invchan, msg, sizeof last_invchan);
   last_invtime = now;
